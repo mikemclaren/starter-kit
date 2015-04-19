78ee38c538627b35e6add663c0927aca81f6c2ee
Mike Mclaren <mike.mclaren@sq1.com>
2015-04-19 14:42:16 -0500

    Added react-express-starter to kits.js.

--- kits.js
+++ kits.js
@@ -1,4 +1,5 @@
 module.exports = {
 	'frontend-starter': 'git@github.com:sq1agency/frontend-starter.git',
-	'react-starter': 'git@github.com:sq1agency/react-starter.git'
+	'react-starter': 'git@github.com:sq1agency/react-starter.git',
+	'react-express-starter': 'git@github.com:sq1agency/react-express-starter.git'
 };
