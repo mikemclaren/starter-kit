afcbfeabc5c5cd737c3f954767ee1e12f05c569f
Mike Mclaren <mike.mclaren@sq1.com>
2015-05-05 10:57:01 -0500

    Added express-starter to kit.

--- kits.js
+++ kits.js
@@ -2,5 +2,6 @@ module.exports = {
 	'frontend-starter': 'git@github.com:sq1agency/frontend-starter.git',
 	'react-starter': 'git@github.com:sq1agency/react-starter.git',
 	'react-express-starter': 'git@github.com:sq1agency/react-express-starter.git',
-	'sailsjs-starter': 'git@github.com:sq1agency/sailsjs-starter.git'
+	'sailsjs-starter': 'git@github.com:sq1agency/sailsjs-starter.git',
+	'express-starter': 'git@github.com:sq1agency/express-starter.git'
 };
